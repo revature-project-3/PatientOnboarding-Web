1557c94c012fe592b61dc093a051d915f3165a18
JacobShanklin <Jacob.Shanklin.2@gmail.com>
2019-07-05 15:46:51 -0400

    update login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -33,7 +33,7 @@ export class LoginComponent implements OnInit {
       private authenticationService: AuthenticationService
       ) {
             if (this.authenticationService.currentUserValue) {
-                this.router.navigate(['/']);
+                this.router.navigate(['/home']);
             }
             if (this.route.snapshot.queryParams.registered) {
                 this.success = 'Registration successful';
