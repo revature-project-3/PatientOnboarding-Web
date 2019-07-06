b81d628e87d0e6fb2766f1039a3182d6bd082080
JacobShanklin <Jacob.Shanklin.2@gmail.com>
2019-07-06 18:35:20 -0400

    schedule appointments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { MedicalHistoryComponent } from './medical-history/medical-history.component';
 import { EdituserComponent } from './edituser/edituser.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ScheduleAppointmentComponent } from './schedule-appointment/schedule-appointment.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     RegisterComponent,
     HomePageComponent,
     MedicalHistoryComponent,
-    EdituserComponent
+    EdituserComponent,
+    ScheduleAppointmentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/schedule-appointment/schedule-appointment.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-schedule-appointment',
+  templateUrl: './schedule-appointment.component.html',
+  styleUrls: ['./schedule-appointment.component.css']
+})
+export class ScheduleAppointmentComponent implements OnInit {
+  myForm: FormGroup;
+  constructor(public modalService: NgbActiveModal, private fb: FormBuilder) {
+    this.createForm();
+   }
+
+  ngOnInit() {
+    this.myForm = this.fb.group({
+      chosenDate: [ '', Validators.required ]
+    });
+    this.myForm.setValue({
+      chosenDate: new Date(2017, 4, 1)
+    });
+  }
+  private createForm() {
+    this.myForm = this.fb.group({
+      firstName: '',
+      lastName: '',
+      password: '',
+      city: ''
+    });
+  }
+  closeModal() {
+    this.modalService.close('Modal Closed');
+  }
+  private submitForm() {
+    this.modalService.close(this.myForm.value);
+  }
+}
